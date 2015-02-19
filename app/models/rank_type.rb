6411daf34cf9aeafd18f146204c3a082fbdb949b
Jordan Smith <jordan.r.smith@gmail.com>
2015-02-19 13:23:36 -0500

    Made select list for rank types.

--- app/models/rank_type.rb
+++ app/models/rank_type.rb
@@ -1,11 +1,11 @@
 class RankType < ActiveRecord::Base
-
+  
   def self.select_list
     select_list_array = []
-    all.order(:name).each do |rank_type|
-      select_list_array << [rank_type.name, rank_type.id]
+    all.order(:name).each do |item|
+      select_list_array << [item.name, item.id]
     end
     select_list_array
-  end 
+  end
 
 end
