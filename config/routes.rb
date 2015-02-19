11146d2bbffce163a386315a1c9dbf90e362ea88
Kyle Dornblaser <kdornblaser@gmail.com>
2015-02-19 15:33:12 -0500

    Add search box to every page

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
-  
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}
-  
+
   root 'games#index'
-  
-  get   'games',              to: 'games#index',      as: 'games_index'
-  get   ':id',                to: 'games#show',       as: 'game'
-  post  'games/new',          to: 'games#create',     as: 'new_game'
-  get   'games/approve/:id',  to: 'games#approve',    as: 'approve_game'
-  get   'games/reject/:id',   to: 'games#reject',     as: 'reject_game'
-  
-  post  'max_ranks/new',      to: 'max_ranks#create', as: 'new_max_rank'
-  
+
+  get   'search',             to: 'search#search',              as: 'search'
+  get   'search-items',       to: 'search#autocomplete',        as: 'search_autocomplete'
+
+  get   'games',              to: 'games#index',                as: 'games_index'
+  get   ':id',                to: 'games#show',                 as: 'game'
+  post  'games/new',          to: 'games#create',               as: 'new_game'
+  get   'games/approve/:id',  to: 'games#approve',              as: 'approve_game'
+  get   'games/reject/:id',   to: 'games#reject',               as: 'reject_game'
+
+  post  'max_ranks/new',      to: 'max_ranks#create',           as: 'new_max_rank'
+
 end
