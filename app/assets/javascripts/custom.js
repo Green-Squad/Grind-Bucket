efc7b800e8918a9b3e99fdf8bea0f3d1c4450865
Jordan Smith <jordan.r.smith@gmail.com>
2015-03-11 14:14:15 -0400

    Add jQuery Swipe plugin for side panel menu.

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,229 +1,197 @@
-$(function(){
+$(function () {
 
-	
-	/*$('.dropdown-menu').click(function(event){
-	  event.stopPropagation();
-	});*/
-	
-	
-	
-	
-	/********************************
-	Toggle Aside Menu
-	********************************/
-	
-	$('.navbar-toggle').click(function() {
-		$('aside.left-panel').toggleClass('collapsed');
-        if ($(document).width() <= 768 ) {
-            $.cookie('collapsed', false, { expires: 7 });
+    /********************************
+     Jquery Swipe
+     ********************************/
+
+    $(window)
+        .on('swipeleft', function(e) {
+            console.log('swipe left')
+            $('.left-panel').addClass('collapsed');
+        })
+        .on('swiperight', function(e) {
+            console.log('swipe right')
+            $('.left-panel').removeClass('collapsed');
+        });
+
+    /********************************
+     Toggle Aside Menu
+     ********************************/
+
+    $('.navbar-toggle').click(function () {
+        $('aside.left-panel').toggleClass('collapsed');
+        if ($(document).width() <= 768) {
+            $.cookie('collapsed', false, {expires: 7});
         } else {
-            if($('aside.left-panel').hasClass('collapsed')) {
-                $.cookie('collapsed', true, { expires: 7 });
+            if ($('aside.left-panel').hasClass('collapsed')) {
+                $.cookie('collapsed', true, {expires: 7});
             } else {
-                $.cookie('collapsed', false, { expires: 7 });
+                $.cookie('collapsed', false, {expires: 7});
             }
         }
-	});
-	
-	
-	
-	
-	
-	/********************************
-	Aside Navigation Menu
-	********************************/
+    });
 
-	$("aside.left-panel nav.navigation > ul > li:has(ul) > a").click(function(){
-		
-		if( $("aside.left-panel").hasClass('collapsed') == false || $(window).width() < 768 ){
+    /********************************
+     Aside Navigation Menu
+     ********************************/
 
-		
-		
-		$("aside.left-panel nav.navigation > ul > li > ul").slideUp(300);
-		$("aside.left-panel nav.navigation > ul > li").removeClass('active');
-		
-		if(!$(this).next().is(":visible"))
-		{
-			
-			$(this).next().slideToggle(300,function(){ $("aside.left-panel:not(.collapsed)").getNiceScroll().resize(); });
-			$(this).closest('li').addClass('active');
-		}
-		
-		return false;
-		
-		}
-		
-	});
-	
-	
-	
-	/********************************
-	popover
-	********************************/
-	if( $.isFunction($.fn.popover) ){
-	$('.popover-btn').popover();
-	}
-	
-	
-	
-	/********************************
-	tooltip
-	********************************/
-	if( $.isFunction($.fn.tooltip) ){
-	$('.tooltip-btn').tooltip()
-	$('[data-toggle="tooltip"]').tooltip()
-	}
-	
-	
-	
-	/********************************
-	NanoScroll - fancy scroll bar
-	********************************/
-	if( $.isFunction($.fn.niceScroll) ){
-	$(".nicescroll").niceScroll({
-	
-		cursorcolor: '#9d9ea5',
-		cursorborderradius : '0px'		
-		
-	});
-	}
-	
+    $("aside.left-panel nav.navigation > ul > li:has(ul) > a").click(function () {
 
-	if( $.isFunction($.fn.niceScroll) ){
-	$("aside.left-panel:not(.collapsed)").niceScroll({
-		cursorcolor: '#8e909a',
-		cursorborder: '0px solid #fff',
-		cursoropacitymax: '0.5',
-		cursorborderradius : '0px'	
-	});
-	}
+        if ($("aside.left-panel").hasClass('collapsed') == false || $(window).width() < 768) {
 
-	
-	
-	
-	
-	/********************************
-	Input Mask
-	********************************/
-	if( $.isFunction($.fn.inputmask) ){
-		$(".inputmask").inputmask();
-	}
-	
-	
-	
-	
-	
-	/********************************
-	TagsInput
-	********************************/
-	if( $.isFunction($.fn.tagsinput) ){
-		$('.tagsinput').tagsinput();
-	}
-	
-	
-	
-	
-	
-	/********************************
-	Chosen Select
-	********************************/
-	if( $.isFunction($.fn.chosen) ){
-		$('.chosen-select').chosen();
-        $('.chosen-select-deselect').chosen({ allow_single_deselect: true });
-	}
-	
-	
-	
-	
-	/********************************
-	DateTime Picker
-	********************************/
-	if( $.isFunction($.fn.datetimepicker) ){
-		$('#datetimepicker').datetimepicker();
-		$('#datepicker').datetimepicker({pickTime: false});
-		$('#timepicker').datetimepicker({pickDate: false});
-		
-		$('#datetimerangepicker1').datetimepicker();
-		$('#datetimerangepicker2').datetimepicker();
-		$("#datetimerangepicker1").on("dp.change",function (e) {
-		   $('#datetimerangepicker2').data("DateTimePicker").setMinDate(e.date);
-		});
-		$("#datetimerangepicker2").on("dp.change",function (e) {
-		   $('#datetimerangepicker1').data("DateTimePicker").setMaxDate(e.date);
-		});
-	}
-	
-	
-	/********************************
-	wysihtml5
-	********************************/
-	if( $.isFunction($.fn.wysihtml5) ){
-		$('.wysihtml').wysihtml5();
-	}
-	
-	
-	
-	/********************************
-	wysihtml5
-	********************************/
-	if( $.isFunction($.fn.ckeditor) ){
-	CKEDITOR.disableAutoInline = true;
-	$('#ckeditor').ckeditor();
-	$('.inlineckeditor').ckeditor();
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	/********************************
-	Scroll To Top
-	********************************/
-	$('.scrollToTop').click(function(){
-		$('html, body').animate({scrollTop : 0},800);
-		return false;
-	});
-	
-	
-	
+
+            $("aside.left-panel nav.navigation > ul > li > ul").slideUp(300);
+            $("aside.left-panel nav.navigation > ul > li").removeClass('active');
+
+            if (!$(this).next().is(":visible")) {
+
+                $(this).next().slideToggle(300, function () {
+                    $("aside.left-panel:not(.collapsed)").getNiceScroll().resize();
+                });
+                $(this).closest('li').addClass('active');
+            }
+
+            return false;
+
+        }
+
+    });
+
+    /********************************
+     popover
+     ********************************/
+
+    if ($.isFunction($.fn.popover)) {
+        $('.popover-btn').popover();
+    }
+
+
+    /********************************
+     tooltip
+     ********************************/
+
+    if ($.isFunction($.fn.tooltip)) {
+        $('.tooltip-btn').tooltip()
+        $('[data-toggle="tooltip"]').tooltip()
+    }
+
+    /********************************
+     NanoScroll - fancy scroll bar
+     ********************************/
+
+    if ($.isFunction($.fn.niceScroll)) {
+        $(".nicescroll").niceScroll({
+
+            cursorcolor: '#9d9ea5',
+            cursorborderradius: '0px'
+
+        });
+    }
+
+    if ($.isFunction($.fn.niceScroll)) {
+        $("aside.left-panel:not(.collapsed)").niceScroll({
+            cursorcolor: '#8e909a',
+            cursorborder: '0px solid #fff',
+            cursoropacitymax: '0.5',
+            cursorborderradius: '0px'
+        });
+    }
+
+    /********************************
+     Input Mask
+     ********************************/
+    if ($.isFunction($.fn.inputmask)) {
+        $(".inputmask").inputmask();
+    }
+
+
+    /********************************
+     TagsInput
+     ********************************/
+
+    if ($.isFunction($.fn.tagsinput)) {
+        $('.tagsinput').tagsinput();
+    }
+
+
+    /********************************
+     Chosen Select
+     ********************************/
+    if ($.isFunction($.fn.chosen)) {
+        $('.chosen-select').chosen();
+        $('.chosen-select-deselect').chosen({allow_single_deselect: true});
+    }
+
+    /********************************
+     DateTime Picker
+     ********************************/
+
+    if ($.isFunction($.fn.datetimepicker)) {
+        $('#datetimepicker').datetimepicker();
+        $('#datepicker').datetimepicker({pickTime: false});
+        $('#timepicker').datetimepicker({pickDate: false});
+
+        $('#datetimerangepicker1').datetimepicker();
+        $('#datetimerangepicker2').datetimepicker();
+        $("#datetimerangepicker1").on("dp.change", function (e) {
+            $('#datetimerangepicker2').data("DateTimePicker").setMinDate(e.date);
+        });
+        $("#datetimerangepicker2").on("dp.change", function (e) {
+            $('#datetimerangepicker1').data("DateTimePicker").setMaxDate(e.date);
+        });
+    }
+
+    /********************************
+     wysihtml5
+     ********************************/
+    if ($.isFunction($.fn.wysihtml5)) {
+        $('.wysihtml').wysihtml5();
+    }
+
+
+    /********************************
+     wysihtml5
+     ********************************/
+
+    if ($.isFunction($.fn.ckeditor)) {
+        CKEDITOR.disableAutoInline = true;
+        $('#ckeditor').ckeditor();
+        $('.inlineckeditor').ckeditor();
+    }
+
+    /********************************
+     Scroll To Top
+     ********************************/
+
+    $('.scrollToTop').click(function () {
+        $('html, body').animate({scrollTop: 0}, 800);
+        return false;
+    });
+
 
 });
 
 
-
-
-
-
-
-
 /********************************
-Toggle Full Screen
-********************************/
+ Toggle Full Screen
+ ********************************/
 
 function toggleFullScreen() {
-	if ((document.fullScreenElement && document.fullScreenElement !== null) || (!document.mozFullScreen && !document.webkitIsFullScreen)) {
-		if (document.documentElement.requestFullScreen) {
-			document.documentElement.requestFullScreen();
-		} else if (document.documentElement.mozRequestFullScreen) {
-			document.documentElement.mozRequestFullScreen();
-		} else if (document.documentElement.webkitRequestFullScreen) {
-			document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
-		}
-	} else {
-		if (document.cancelFullScreen) {
-			document.cancelFullScreen();
-		} else if (document.mozCancelFullScreen) {
-			document.mozCancelFullScreen();
-		} else if (document.webkitCancelFullScreen) {
-			document.webkitCancelFullScreen();
-		}
-	}
+    if ((document.fullScreenElement && document.fullScreenElement !== null) || (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+        if (document.documentElement.requestFullScreen) {
+            document.documentElement.requestFullScreen();
+        } else if (document.documentElement.mozRequestFullScreen) {
+            document.documentElement.mozRequestFullScreen();
+        } else if (document.documentElement.webkitRequestFullScreen) {
+            document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+        }
+    } else {
+        if (document.cancelFullScreen) {
+            document.cancelFullScreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitCancelFullScreen) {
+            document.webkitCancelFullScreen();
+        }
+    }
 }
-
-
-
-
