2077d375cb63bb517abd9f17d957435abc74639c
Kyle <kdornblaser@gmail.com>
2015-03-12 13:14:08 -0400

    Update Google Recaptcha site key for new domain.

--- app/assets/javascripts/show_forms.js
+++ app/assets/javascripts/show_forms.js
@@ -1,10 +1,13 @@
 $(function() {
+
+    var siteKey = '6LeuowMTAAAAAOJKRwJA2BzhubFz-QJtivjb_myS'
+
   $('#add-game-button').click(function() {
     $('#add-game-button').addClass('hidden');
     $('#cancel-add-game-button').removeClass('hidden');
     $('#add-game-box').removeClass('hidden');
     grecaptcha.render('recaptcha-game', {
-      'sitekey' : '6LejWAETAAAAAJiT5Y2l1h9NLIRmwLjMd2ThoJzt',
+      'sitekey' : siteKey,
       'theme' : 'light'
     });
   });
@@ -20,7 +23,7 @@ $(function() {
     $('#cancel-add-max-rank-button').removeClass('hidden');
     $('#add-max-rank-box').removeClass('hidden');
     grecaptcha.render('recaptcha-max-rank', {
-      'sitekey' : '6LejWAETAAAAAJiT5Y2l1h9NLIRmwLjMd2ThoJzt',
+      'sitekey' : siteKey,
       'theme' : 'light'
     });
   });
